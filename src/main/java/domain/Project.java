15681b409792ad5e4fcb7623f19a584ecf3ded2c
vicent <forjunkmailonlyelv@gmail.com>
2025-04-28 16:04:43 +0200

    Same as before

--- src/main/java/domain/Project.java
+++ src/main/java/domain/Project.java
@@ -7,11 +7,19 @@ public class Project {
     private String projectID;
     private String projectName;
     private String projectManager;
+    private Double totalHourAmount;
 
     private final String[] availableChars = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"};
     private final String availableNumbs = "1234567890";
 
     private final Random random = new Random();
+
+    public Project(String projectName) {
+        this.projectID = createID();
+        this.projectName = projectName;
+        this.projectManager = "Unassigned";
+        this.totalHourAmount = 0.0;
+    }
 
     private String createID() {
         // Combine letters and numbers into one array
@@ -24,7 +32,7 @@ public class Project {
         for (int i = 0; i < availableNumbs.length(); i++) {
             allChars[availableChars.length + i] = String.valueOf(availableNumbs.charAt(i));
         }
-        
+
         // Generate ID of 6 characters
         StringBuilder id = new StringBuilder();
         for (int i = 0; i < 16; i++) {
@@ -34,13 +42,24 @@ public class Project {
         return id.toString();
     }
 
-    public Project(String projectName) {
-        this.projectID = createID();
-        this.projectName = projectName;
-        this.projectManager = "Not assigned";
-    }
-
     public String getProjectID() {
         return projectID;
+    }  
+    public String getProjectName() {
+        return projectName;
+    }
+    public String getProjectManager() {
+        return projectManager;
+    }
+    public Double getTotalHourAmount() {
+        return totalHourAmount;
+    }
+
+    // Changing
+    public void setProjectManager(String projectManager) {
+        this.projectManager = projectManager;
+    }
+    public void setTotalHourAmount(Double totalHourAmount) {
+        this.totalHourAmount = totalHourAmount;
     }
 }
